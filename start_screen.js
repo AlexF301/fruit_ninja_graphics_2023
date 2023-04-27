ac53c1ea7990127c8359ba26bb8d6ea2cbe60eab
Edwin <cojitamboe@moravian.edu>
2023-04-27 13:04:19 -0400

    Added welcome screen

--- start_screen.js
+++ start_screen.js
@@ -2,8 +2,8 @@
 // AUTHORS: Alexander Flores Sosa and Edwin Cojitambo
 
 /**
- * Once the user clicks the play button, the difficulty chosen is saved and passed
- * to the game logic through local storage
+ * Once the user clicks the play button, the difficulty chosen is saved through local
+ * storage, and used in the game file
  */
 window.addEventListener('load', function init() {
     document.getElementById('start-game').addEventListener("click", function () {
